c3b41a5b437c68bfd0d3d428feef2b273d10916b
lixianle <1208884260@qq.com>
2022-03-14 20:02:17 +0800

    项目目录设置

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <nav-header></nav-header>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+import NavHeader from '../components/NavHeader.vue'
+import NavFooter from '../components/NavFooter.vue'
+export default {
+  name: "Home",
+  components: {
+    NavHeader,
+    NavFooter
+  }
+};
+</script>
